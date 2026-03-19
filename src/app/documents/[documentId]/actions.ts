82e25c59832548a5323b2c8b13c193e8f17dbef6
Parth Jadhao <parthjadhao01@gmail.com>
2026-03-19 16:35:57 +0530

    feat : implemented notification module

--- src/app/documents/[documentId]/actions.ts
+++ src/app/documents/[documentId]/actions.ts
@@ -1,18 +1,23 @@
 "use server"
 
 import {auth, clerkClient} from "@clerk/nextjs/server";
+import {ConvexHttpClient} from "convex/browser";
+import {Id} from "../../../../convex/_generated/dataModel"
+import {api} from "../../../../convex/_generated/api";
+
+const convex = new ConvexHttpClient(process.env.NEXT_PUBLIC_CONVEX_URL!)
+
+export async function getDocuments(ids : Id<"documents">[]){
+    return await convex.query(api.documents.getByIds,{ids})
+}
 
 export async function getUsers(){
-    console.log("function get user calling")
     const { sessionClaims } = await auth();
     const clerk = await clerkClient();
     const response = await clerk.users.getUserList({
         // @ts-ignore
         organizationId : [sessionClaims?.o?.id as string]
     })
-
-    console.log("users",response)
-
     return response.data.map((user) => ({
         id: user.id,
         name: user.fullName ?? user.primaryEmailAddress?.emailAddress ?? "Anonymous",
